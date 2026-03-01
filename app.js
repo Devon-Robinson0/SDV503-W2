274a507be0ff2674b6ca6be39aed6e0b44de0732
Devon-Robinson0 <devon-robinson2@live.nmit.ac.nz>
2026-03-02 10:40:35 +1300

    Added basic understanding code

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 console.log("Hello, World!");
 console.log("This is a simple JavaScript file.");
+
+var name = "Devon"; // function - scoped, hoisted - avoid in modern JS
+let age = 17; // Block - scoped, can be reassigned
+const PI = 3.14; // Block - scoped, cannot be reassigned
+
+let score = 0;
+score = 10; // Ok - reassignment allowed for let
+
+const MAX = 100;
+//Max = 200; wont work because its a const variable // TypeError; assignment to constant variable.
