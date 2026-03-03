61a120c3a64618649905dd82839a8331ec01e6e2
Devon-Robinson0 <devon-robinson2@live.nmit.ac.nz>
2026-03-04 12:18:48 +1300

    added new version of string

--- app.js
+++ app.js
@@ -1,32 +1,74 @@
-console.log("Hello, World!");
-console.log("This is a simple JavaScript file.");
+// console.log("Hello, World!");
+// console.log("This is a simple JavaScript file.");
 
-var name = "Devon"; // function - scoped, hoisted - avoid in modern JS
-let age = 17; // Block - scoped, can be reassigned
-const PI = 3.14; // Block - scoped, cannot be reassigned
+// var name = "Devon"; // function - scoped, hoisted - avoid in modern JS
+// let age = 17; // Block - scoped, can be reassigned
+// const PI = 3.14; // Block - scoped, cannot be reassigned
 
-let score = 0;
-score = 10; // Ok - reassignment allowed for let
+// let score = 0;
+// score = 10; // Ok - reassignment allowed for let
 
-const MAX = 100;
-//Max = 200; wont work because its a const variable // TypeError; assignment to constant variable.
+// const MAX = 100;
+// //Max = 200; wont work because its a const variable // TypeError; assignment to constant variable.
 
-let homeAddress = "5 Kingi Place, Richmond"; //camelCase
-let userAge = 32;
-console.log(typeof userAge);
+// let homeAddress = "5 Kingi Place, Richmond"; //camelCase
+// let userAge = 32;
+// console.log(typeof userAge);
 
-console.log(2 ** 3);
-console.log(5 === 5);
+// console.log(2 ** 3);
+// console.log(5 === 5);
 
-console.log(5 === "5");
-console.log(5 == "5");
+// console.log(5 === "5");
+// console.log(5 == "5");
 
-console.log(5 !== 5);
-console.log(5 <= 5);
+// console.log(5 !== 5);
+// console.log(5 <= 5);
 
-let bool1 = false;
-let bool2 = false;
+// let bool1 = false;
+// let bool2 = false;
 
-if (!bool1 || bool2) {
-    console.log("At least one is true");
-}
+// if (!bool1 || bool2) {
+//     console.log("At least one is true");
+// }
+
+const msg = "Hello, Javascript!";
+
+console.log(`The length of msg is = ${msg.length}`);
+
+console.log(msg.toUpperCase());
+console.log(msg.replace("Hello", "Hi"));
+
+msg.length; // 18
+msg.toUpperCase(); // "HELLO, JAVASCRIPT!"
+msg.toLowerCase(); // "hello, javascript!"
+msg.includes("Java"); // true
+msg.startsWith("He"); // true
+msg.indexOf("Java"); // 7
+msg.slice(7, 17); // "Javascript"
+msg.replace("Hello", "Hi"); // "Hi, Javascript!"
+msg.split(","); // ["Hello", " Javascript!"]
+" hi ".trim(); // "hi"
+
+// Strings are zero indexed: "Hello"[0] === "H", "Hello"[4] === "o", etc.
+
+const name = "Devon";
+const age = 25;
+
+// Old way - concatanation (error prone)
+const msg1 = "Hello, " + name + "! You are " + age + " years old.";
+
+//New way - template literals (clean and readable
+const msg2 = `Hello, ${name}! You are ${age} years old.`;
+
+// //Multi line strings
+// const html =
+//     <div class="card">
+//         <h2>${name}</h2>
+//         <p>Age: ${age}</p>
+//     </div>
+// ;
+
+// // Any expression works inside ${}
+// const total = `Price: ${(12.99 * 3).toFixed(2)}`; // "Price: 38.97"
+
+console.log(`Hello my name is ${name} im ${age} years old`); // This will not work as expected - it will print the literal string with ${name} and ${age}:
