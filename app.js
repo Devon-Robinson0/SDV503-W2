b22377ec12882f28e9a8ae048d3edf00c274880f
Devon-Robinson0 <devon-robinson2@live.nmit.ac.nz>
2026-03-04 12:51:53 +1300

    added if / else if / else / switch statements

--- app.js
+++ app.js
@@ -72,3 +72,33 @@ const msg2 = `Hello, ${name}! You are ${age} years old.`;
 // const total = `Price: ${(12.99 * 3).toFixed(2)}`; // "Price: 38.97"
 
 console.log(`Hello my name is ${name} im ${age} years old`); // This will not work as expected - it will print the literal string with ${name} and ${age}:
+
+let fullName = "Devon Robinson";
+
+fullName = "Jack Smith";
+
+// if / else if / else
+const score = 75;
+if (score >= 90) {
+    console.log("A");
+} else if (score >= 80) {
+    console.log("B");
+} else if (score >= 70) {
+    console.log("C");
+} else {
+    console.log("Below C");
+}
+
+// switch - great for exact matches
+const day = "Monday";
+switch (day) {
+    case "Monday":
+    case "Tuesday":
+        console.log("Start of the week");
+        break;
+    case "Friday":
+        console.log("TGIF!");
+        break;
+    default:
+        console.log("Midweek");
+}
